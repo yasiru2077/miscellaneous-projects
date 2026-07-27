fd2f928a85fdca37d61ca5db71031090b768e7fc
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2026-07-27 17:44:24 +0530

    task

--- task-client/src/store/authStore.ts
+++ task-client/src/store/authStore.ts
@@ -1,4 +1,6 @@
-import type { LoginRequest } from "../types";
+import { persist } from "zustand/middleware";
+import type { LoginResponse, RegisterResponse } from "../types";
+import { create } from "zustand";
 
 type AuthUser = {
   userId: string;
@@ -10,19 +12,45 @@ interface AuthState {
   user: AuthUser | null;
   token: string | null;
   isAuthenticated: boolean;
-  setAuth: (data: LoginRequest | LoginRequest) => void;
+  setAuth: (data: LoginResponse | RegisterResponse) => void;
   clearAuth: () => void;
 }
 
-// export const useAuthStore = create<AuthState>()(
-//   persist((set) => ({
-//     user: null,
-//     token: null,
-//     isAuthenticated: false,
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
 
-//     setAuth:(data) => {
+      setAuth: (data) => {
+        localStorage.setItem("task_token", data.accessToken);
 
-//     }
+        set({
+          token: data.accessToken,
+          user: {
+            userId: data.userId,
+            username: data.username,
+            email: data.email,
+          },
+          isAuthenticated: true,
+        });
+      },
 
-//   })),
-// );
+      clearAuth: () => {
+        localStorage.removeItem("task_token");
+        localStorage.removeItem("task_user");
+        set({ user: null, token: null, isAuthenticated: false });
+      },
+    }),
+    {
+      name: "task_user",
+      partialize: (state) => ({
+        user: state.user,
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+      }),
+    },
+  ),
+);
+
